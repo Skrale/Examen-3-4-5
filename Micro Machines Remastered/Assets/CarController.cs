853437536f2dc82b7ed0646302c4603c11a7cd35
Joey Zwart <joeyzwart@live.nl>
2018-03-27 13:21:21 +0200

    Added car controls and race track

---
+++ Micro Machines Remastered/Assets/CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    float speedForce = 100.0f;
+    float torque = 15.0f;
+    
+    void Start ()
+    {
+		
+	}
+	
+	void FixedUpdate ()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        float turn = Input.GetAxis("Horizontal");
+
+        if (Input.GetButton("Accelerate"))
+        {
+            rb.AddForce(transform.forward * speedForce);
+        }
+
+        rb.AddTorque(transform.up * torque * turn);
+
+
+	}
+
+    Vector3 ForwardVelocity()
+    {
+        return transform.forward * Vector3.Dot(GetComponent<Rigidbody>().velocity, transform.forward);
+    }
+
+    Vector3 RightVelocity()
+    {
+        return transform.forward * Vector3.Dot(GetComponent<Rigidbody>().velocity, transform.forward);
+    }
+}
