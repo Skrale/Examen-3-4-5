86cdc0f828599f85f204cef335c904cb56c97bb2
Joey Zwart <joeyzwart@live.nl>
2018-03-28 13:55:19 +0200

    you are now able to fall off the track, still have to add a respawn mechanic

--- Micro Machines Remastered/Assets/CarController.cs
+++ Micro Machines Remastered/Assets/CarController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
+    float shrinkSpeed = 1.0f;
+    Vector3 targetScale = new Vector3(0.1f, 0.1f, 0.1f);
+    float destroyScale = 0.15f;
+    bool hasCollided = false;
+    GameObject car;
+    Vector3 standardTransform;
+
     float speedForce = 60.0f;
     float brakeForce = 5.0f;
     float torqueForce = 60.0f;
@@ -12,12 +19,34 @@ public class CarController : MonoBehaviour {
     float maxStickyVelocity = 2.5f;
     float minSlippyVelocity = 1.5f;
 
+    public GameObject[] outOfBounds;
+
     void Start ()
     {
-		
+        car = this.gameObject;
+        Debug.Log(car.transform.localScale);
+        standardTransform = car.transform.localScale;
 	}
-	
-	void FixedUpdate ()
+
+    void Update()
+    {
+        if (hasCollided)
+        {
+            car.transform.localScale -= Vector3.one * Time.deltaTime * shrinkSpeed;
+
+            if (car.transform.localScale.x < targetScale.x && car.transform.localScale.y < targetScale.y && car.transform.localScale.z < targetScale.z)
+            {
+                hasCollided = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            car.transform.localScale = standardTransform;
+        }
+    }
+
+    void FixedUpdate ()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
 
@@ -53,5 +82,10 @@ public class CarController : MonoBehaviour {
     Vector3 RightVelocity()
     {
         return transform.right * Vector3.Dot(GetComponent<Rigidbody>().velocity, transform.right);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hasCollided = true;
     }
 }
