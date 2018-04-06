2609b0cc933b23b7182c20cd7700a61ac5c161de
Joey Zwart <joeyzwart@live.nl>
2018-04-06 10:54:31 +0200

    added level 2 to the menu

--- Micro Machines Remastered/Assets/Scripts/MainMenu.cs
+++ Micro Machines Remastered/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
     public void ExitGame()
     {
         Debug.Log("Exit");
